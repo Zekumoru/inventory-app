d7ee2846e6c888ae4506d8f0fd9cbb154eecd702
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-26 22:18:38 +0100

    Account for invalid id in item detail page

--- controllers/itemController.ts
+++ controllers/itemController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import Item, { IItem } from "../models/Item";
 import { IdRequest } from "../types/controller";
+import { isValidObjectId } from "mongoose";
 
 // Types for item list
 interface ItemListLocals extends Record<string, any> {
@@ -34,22 +35,35 @@ interface ItemDetailResponse extends Omit<Response<{}, Partial<ItemDetailLocals>
 };
 
 // Display detail page for an item
-export const item_detail = asyncHandler(async (req: IdRequest, res: ItemDetailResponse, next: NextFunction) => {
-  const item = await Item.findById<IItem>(req.params.id).exec();
+export const item_detail = [
+  asyncHandler(async (req: IdRequest, res: ItemDetailResponse, next: NextFunction) => {
+    if (!isValidObjectId(req.params.id)) {
+      // Invalid item id
+      res.status(400);
+      return res.render('item_detail', {
+        title: 'Invalid item id',
+        item: null,
+      });
+    }
 
-  if (item === null) {
-    res.status(404);
-    return res.render('item_detail', {
-      title: 'Item not found',
+    const item = await Item.findById<IItem>(req.params.id).exec();
+
+    if (item === null) {
+      // Item does not exist
+      res.status(404);
+      return res.render('item_detail', {
+        title: 'Item not found',
+        item: null,
+      });
+    }
+
+    // Render item
+    res.render('item_detail', {
+      title: `Item: ${item.name}`,
       item,
-    });
-  }
-
-  res.render('item_detail', {
-    title: `Item: ${item.name}`,
-    item,
-  })
-});
+    })
+  }),
+];
 
 // Display create form on GET
 export const item_create_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
