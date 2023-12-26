32f5e04695a8589476e14775f94a2b6fc9dc860b
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-26 20:41:59 +0100

    Create index view and modify error view

--- controllers/indexController.ts
+++ controllers/indexController.ts
@@ -1,6 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
+import Category, { ICategory } from "../models/Category";
+import Item from "../models/Item";
 
-export const index = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  res.render('index', { title: 'Express ' });
+interface IndexLocals extends Record<string, any> {
+  title?: string;
+  categories?: ICategory[];
+  categoriesCount?: number,
+  itemsCount?: number,
+}
+
+interface IndexResponse extends Omit<Response<{}, IndexLocals>, 'render'> {
+  render: (view: string, options?: Required<IndexLocals>) => void;
+};
+
+// Display home page
+export const index = asyncHandler(async (req: Request, res: IndexResponse, next: NextFunction) => {
+  const categories = await Category.find<ICategory>().sort({ name: 1 }).exec();
+  const itemsCount = await Item.countDocuments().exec();
+
+  res.render('index', {
+    title: 'Inventory App!',
+    categoriesCount: categories.length,
+    categories,
+    itemsCount,
+  });
 });
