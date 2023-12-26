192606e1d62e1f9767419dcbb336359f875ad933
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-26 21:11:21 +0100

    Create category list view

--- controllers/categoryController.ts
+++ controllers/categoryController.ts
@@ -1,9 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
+import Category, { ICategory } from "../models/Category";
+
+// Types for category list
+interface CategoryListLocals extends Record<string, any> {
+  title?: string;
+  categories?: ICategory[];
+}
+
+interface CategoryListResponse extends Omit<Response<{}, CategoryListLocals>, 'render'> {
+  render: (view: string, options?: Required<CategoryListLocals>) => void;
+};
 
 // Display all categories
-export const category_list = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  throw new Error('Not implemented yet!');
+export const category_list = asyncHandler(async (req: Request, res: CategoryListResponse, next: NextFunction) => {
+  const categories = await Category.find<ICategory>().sort({ name: 1 }).exec();
+
+  res.render('category_list', {
+    title: 'Categories',
+    categories,
+  });
 });
 
 // Display detail page for a category
