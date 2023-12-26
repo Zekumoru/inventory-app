55b185d30e0a875547eb6a69fa4cb9fad60a9fa4
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-26 19:59:48 +0100

    Fix missing async keyword in controllers

--- controllers/categoryController.ts
+++ controllers/categoryController.ts
@@ -2,41 +2,41 @@ import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 
 // Display all categories
-export const category_list = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_list = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display detail page for a category
-export const category_detail = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_detail = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display create form on GET
-export const category_create_get = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_create_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Handle creating category on POST
-export const category_create_post = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_create_post = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display delete page on GET
-export const category_delete_get = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_delete_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Handle deleting category on POST
-export const category_delete_post = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_delete_post = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display update page on GET
-export const category_update_get = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_update_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Handle updating category on POST
-export const category_update_post = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const category_update_post = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
--- controllers/itemController.ts
+++ controllers/itemController.ts
@@ -2,41 +2,41 @@ import { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 
 // Display all items
-export const item_list = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_list = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display detail page for an item
-export const item_detail = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_detail = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display create form on GET
-export const item_create_get = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_create_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Handle creating item on POST
-export const item_create_post = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_create_post = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display delete page on GET
-export const item_delete_get = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_delete_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Handle deleting item on POST
-export const item_delete_post = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_delete_post = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Display update page on GET
-export const item_update_get = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_update_get = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
 
 // Handle updating item on POST
-export const item_update_post = asyncHandler((req: Request, res: Response, next: NextFunction) => {
+export const item_update_post = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   throw new Error('Not implemented yet!');
 });
